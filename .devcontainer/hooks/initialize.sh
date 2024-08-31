2d96991e7afc449a0a440f7971d2e183df0e84d9
Kazumasa Hirata <k_hirata@openwork.co.jp>
2024-08-31 01:04:39 +0000

    chore: setup .bash_profile for app container

--- .devcontainer/hooks/initialize.sh
+++ .devcontainer/hooks/initialize.sh
@@ -5,3 +5,13 @@
 # Expected to be run from the root of the repository in local.
 # --------------------------------------------------
 set -e
+
+function expand_tmpl() {
+  local tmpl=$1
+  local dest=$(echo $tmpl | sed 's/\.tmpl$//')
+  if [ ! -f $dest ]; then
+    cp $tmpl $dest
+  fi
+}
+
+expand_tmpl .devcontainer/app/.bash_profile.tmpl
